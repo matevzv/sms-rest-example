9c3a75ca946c48e29dd7996aafdde3476c2600cd
Matevz Vucnik <vucnikm@gmail.com>
2015-06-12 10:13:01 +0200

    ckecks if node/sensor exists

--- sms-rest-api.py
+++ sms-rest-api.py
@@ -1,26 +1,31 @@
+#!/usr/bin/env python
+
 import sys
 import time
+import requests
+import json
 from datetime import datetime
-import requests, json
 
 headers = {'Content-Type': 'application/json', 'Authorization': 'yc92PyLkeBUyqN1msDan6YOCl+IT2u9M'}
 api_url = "http://localhost:3000/api"
 nodes_url = "/nodes"
 sensors_url = "/sensors"
 measurements_url = "/measurements"
-node_name = "test4"
+node_name = "test5"
 sensor_type = "SHT21"
 sensor_quantity = "humidity"
 sensor_unit = "%"
 
-# creates new node
+# creates new node if not exists
 
-json_str = '''{ "name": "''' + node_name + '''", "location": "", "loc_lat": null, "loc_lon": null, "cluster": "1", "cluster_title": "test", "status": "active", "setup": "", "scope": "", "project": "", "user_comment": "", "box_label": "", "serial_no": "", "mac": "", "network_addr": null, "network_addr2": null, "firmware": "", "bootloader": "", "role": "device", "sensors": [], "components": [] }''';
-r = requests.post(api_url+nodes_url, data=json_str, headers=headers)
-print r.text
-if "error" in r.text:
-    print "Error: Node with the name " + node_name + " already exists"
-    sys.exit()
+r = requests.get(api_url+nodes_url+"?name="+node_name, headers=headers)
+if "No nodes found" in r.text:
+    json_str = '''{ "name": "''' + node_name + '''", "location": "", "loc_lat": null, "loc_lon": null, "cluster": "1", "cluster_title": "test", "status": "active", "setup": "", "scope": "", "project": "", "user_comment": "", "box_label": "", "serial_no": "", "mac": "", "network_addr": null, "network_addr2": null, "firmware": "", "bootloader": "", "role": "device", "sensors": [], "components": [] }''';
+    r = requests.post(api_url+nodes_url, data=json_str, headers=headers)
+    print r.text
+    if "error" in r.text:
+        print "Error: Node with the name " + node_name + " already exists"
+        sys.exit()
 
 # gets new node id
 
@@ -29,12 +34,14 @@ new_node_id = [r.json()[0]['_id']]
 r = requests.get(api_url+nodes_url+"/"+new_node_id[0], headers=headers)
 new_node_id.append(str(r.json()[0]['id']))
 
-# makes new sensor
+# makes new sensor if not exists
 
 sensor_id = new_node_id[1]+"-"+sensor_type+"-"+sensor_quantity
-json_str = '''{ "id": "'''+sensor_id+'''", "type": "'''+sensor_type+'''", "quantity": "'''+sensor_quantity+'''", "unit": "'''+sensor_unit+'''", "node_id": "'''+new_node_id[0]+'''", "node": '''+new_node_id[1]+''' }'''
-r = requests.post(api_url+sensors_url, data=json_str, headers=headers)
-print r.text
+r = requests.get(api_url+sensors_url+"?id="+sensor_id, headers=headers)
+if "No sensors found" in r.text:
+    json_str = '''{ "id": "'''+sensor_id+'''", "type": "'''+sensor_type+'''", "quantity": "'''+sensor_quantity+'''", "unit": "'''+sensor_unit+'''", "node_id": "'''+new_node_id[0]+'''", "node": '''+new_node_id[1]+''' }'''
+    r = requests.post(api_url+sensors_url, data=json_str, headers=headers)
+    print r.text
 
 # gets new sensor id
 
